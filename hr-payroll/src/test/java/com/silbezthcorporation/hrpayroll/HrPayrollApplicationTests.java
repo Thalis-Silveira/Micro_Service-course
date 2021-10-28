51778ab464eb788f438c64b0cd72ae66f22e831d
Thalis-Silveira <thalissilbez7@gmail.com>
2021-10-28 15:04:12 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/silbezthcorporation/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silbezthcorporation.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
